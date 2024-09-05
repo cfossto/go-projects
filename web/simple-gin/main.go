7373662235f4421de70f1db5e8500d3d0da2cdfa
Christopher Fossto <70951673+cfossto@users.noreply.github.com>
2024-09-05 13:57:19 +0200

    Update main.go

--- web/simple-gin/main.go
+++ web/simple-gin/main.go
@@ -14,22 +14,18 @@ func main(){
 	
 	// Get-endpoint.
 	r.GET("/hello", func(c *gin.Context) {
-
 		// Make endpoint return status 200 and return a Go map (key/value)
 		c.JSON(200, gin.H{
 			"msg":"working",
 		})
 	})
 
-
 	r.GET("/site", func(ctx *gin.Context) {
-
 		ctx.HTML(http.StatusOK,"index.tmpl", gin.H{
 			"firstHeading":"Welcome",
 		},)
-
 	})
 
+	
 	r.Run()
-
 }

a13958058e539b086db7f91448b2c8107dcfad8e
Christopher Fossto <70951673+cfossto@users.noreply.github.com>
2024-09-05 11:25:42 +0200

    Simple Gin and first commit.

---
+++ web/simple-gin/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main(){
+
+	// Default router.
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	
+	// Get-endpoint.
+	r.GET("/hello", func(c *gin.Context) {
+
+		// Make endpoint return status 200 and return a Go map (key/value)
+		c.JSON(200, gin.H{
+			"msg":"working",
+		})
+	})
+
+
+	r.GET("/site", func(ctx *gin.Context) {
+
+		ctx.HTML(http.StatusOK,"index.tmpl", gin.H{
+			"firstHeading":"Welcome",
+		},)
+
+	})
+
+	r.Run()
+
+}
